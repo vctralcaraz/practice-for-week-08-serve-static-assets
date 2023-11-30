726a3988e09d70c25b4d09309c03fa970b60fd88
Victor Alcaraz <vctralcaraz@gmail.com>
2023-11-29 21:52:51 -0800

    phase 2 complete

--- server.js
+++ server.js
@@ -3,12 +3,38 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
-  const fileContents = fs.readFileSync('./index.html', 'utf-8');
+  const subs = req.url.split('/');
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
+  if(req.url.startsWith('/static')) {
+    if(req.method === 'GET') {
+      if(subs[2] === 'images') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'image/jpeg');
 
-  res.end(fileContents);
+        const fileContents = fs.readFileSync(`./assets/${subs[2]}/${subs[3]}`);
+
+        return res.end(fileContents);
+      }
+      if(subs[2] === 'css') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/css');
+
+        const fileContents = fs.readFileSync(`./assets/${subs[2]}/${subs[3]}`);
+
+        return res.end(fileContents);
+      }
+    }
+  }
+
+  if(req.method === 'GET' && req.url === '/') {
+    const fileContents = fs.readFileSync('./index.html', 'utf-8');
+  
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+  
+    return res.end(fileContents);
+  }
+
 });
 
 const port = 5000;
