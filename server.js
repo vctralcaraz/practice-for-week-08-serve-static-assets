834e2feaad6dbc10705b889d38377848e37202cf
Victor Alcaraz <vctralcaraz@gmail.com>
2023-11-29 21:34:10 -0800

    phase 1 complete

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  const fileContents = fs.readFileSync('./index.html', 'utf-8');
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  res.end(fileContents);
 });
 
 const port = 5000;
